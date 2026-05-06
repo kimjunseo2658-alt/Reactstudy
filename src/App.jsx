044bcafd509852389f44d91571bff6ec5797c881
kimjunseo2658 <kimjunseo2658@gmail.com>
2026-05-06 21:37:14 +0900

    5/6 집수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Name from './name';
 import Cal from './Cal';
 
 function App() {
-  const allScore = [100, 90, 80]
+  const allScore = [100, 90, 80, 70, 60]
   return (
     <Cal score = {allScore}/>
   )
@@ -12,7 +12,7 @@ function App() {
 
 export default App
 
-// 1인당 3과목 점수
-// 총 3명이다
+// 1인당 5과목 점수
+// 총 35이다
 // 세트로 묶어서 자식에게 보낸다 
 // 자식은 총점과 평균을 계산한다
--- src/Cal.jsx
+++ src/Cal.jsx
@@ -4,15 +4,19 @@ function Cal(props) {
   const s1 = props.score[0];
   const s2 = props.score[1];
   const s3 = props.score[2];
+  const s4 = props.score[3];
+  const s5 = props.score[4];
 
-  const total = s1 + s2 + s3;
-  const average = total / 3;
+  const total = s1 + s2 + s3 + s4 + s5;
+  const average = total / 5;
    return (
     <>
     <h2>성적표</h2>
     <p>국어: {s1}점</p>
     <p>수학: {s2}점</p>
     <p>영어: {s3}점</p>
+    <p>사회: {s4}점</p>
+    <p>과학: {s5}점</p>
     <hr />
     <p>
      <strong>총점: {total}</strong>
